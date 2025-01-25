5d3e77556ddf6e33444207a8f1d141c7b596e4f7
joy <joywong367@gmail.com>
2025-01-25 10:53:29 +0800

    Add Feature 1
    Load Files (airlines and boarding gates)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,19 @@
-﻿
-using S10269287_PRG2Assignment;
+﻿using S10269287_PRG2Assignment;
+
+//Feature 1 - Loaad Files  (airlines and boarding gates)
+Terminal terminal = new Terminal("Terminal 5");
+using (StreamReader sr = new StreamReader("airlines.csv"))
+{
+    string? s = sr.ReadLine();
+    while ((s = sr.ReadLine()) != null)
+
+    {
+        string[] airlines = s.Split(',');
+        string airlineName = airlines[0];
+        string airlineCode = airlines[1];
+        Airline newAirline = new Airline(airlineName, airlineCode);
+        terminal.AddAirline(newAirline);
+
+
+    }
+}
