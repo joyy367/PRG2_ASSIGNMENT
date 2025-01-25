1b5b35a1208f4360a3f70b72c2bd13b231a812d6
joy <joywong367@gmail.com>
2025-01-25 10:44:48 +0800

    Add DDJBFlight Class
    Subclass of Flight Class

---
+++ DDJBFlight.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace S10269287_PRG2Assignment
+{
+    class DDJBFlight : Flight
+    {
+        public DDJBFlight() { }
+        public DDJBFlight(string flightName, string origin, string destination, DateTime expectedTime) : base(flightName, origin, destination, expectedTime) { }
+        public override double CalculateFees()
+        {
+            double fee = 800 + 300;
+            return fee;
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "Flight Type: DDJB";
+        }
+    }
+}
