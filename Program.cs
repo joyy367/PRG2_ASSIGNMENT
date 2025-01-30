801644d743653d52e35d0e902f3c7b0cedef84bb
joy <joywong367@gmail.com>
2025-01-30 17:40:50 +0800

    Add Feature 2
    Load files (flights)

--- Program.cs
+++ Program.cs
@@ -17,3 +17,38 @@ using (StreamReader sr = new StreamReader("airlines.csv"))
 
     }
 }
+
+//Feature 2 - Load files (flights)
+using (StreamReader sr = new StreamReader("flights.csv"))
+{
+    string? s = sr.ReadLine();
+    while ((s = sr.ReadLine()) != null)
+
+    {
+        string[] flights = s.Split(',');
+        string flightNumber = flights[0];
+        string origin = flights[1];
+        string destination = flights[2];
+        DateTime expectedTime = Convert.ToDateTime(flights[3]);
+        string specialRequestCode = flights[4];
+        Flight newFlight;
+        if (specialRequestCode == "DDJB")
+        {
+            newFlight = new DDJBFlight(flightNumber, origin, destination, expectedTime);
+        }
+        else if (specialRequestCode == "CFFT")
+        {
+            newFlight = new CFFTFlight(flightNumber, origin, destination, expectedTime);
+        }
+        else if (specialRequestCode == "LWTT")
+        {
+            newFlight = new LWTTFlight(flightNumber, origin, destination, expectedTime);
+        }
+        else
+        {
+            newFlight = new NORMFlight(flightNumber, origin, destination, expectedTime);
+        }
+        terminal.Flights[flightNumber] = newFlight;
+
+    }
+}
