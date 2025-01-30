43ff326d9abc199679397c23e3cbeb0f0725e8f3
joy <joywong367@gmail.com>
2025-01-30 17:44:33 +0800

    Update NORMFlight.cs

--- NORMFlight.cs
+++ NORMFlight.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace S10269287_PRG2Assignment
 {
-    internal class NORMFlight
+    class NORMFlight : Flight
+{
+    public NORMFlight() { }
+    public NORMFlight(string flightName, string origin, string destination, DateTime expectedTime) : base(flightName, origin, destination, expectedTime) { }
+
+    public override double CalculateFees()
     {
+        double fee = base.CalculateFees();
+        return fee;
     }
+
+    public override string ToString()
+    {
+        return base.ToString() + "Flight Type: NORM";
+    }
+}
 }
