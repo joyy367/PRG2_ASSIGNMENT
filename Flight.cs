769cd13c1832830cd98a9c353fdf4efb006af12d
joy <joywong367@gmail.com>
2025-01-30 17:43:43 +0800

    Update Flight.cs

--- Flight.cs
+++ Flight.cs
@@ -6,7 +6,30 @@ using System.Threading.Tasks;
 
 namespace S10269287_PRG2Assignment
 {
-    internal class Flight
+    class Flight
+{
+    public string FlightNumber { get; set; }
+    public string Origin { get; set; }
+    public string Destination { get; set; }
+    public DateTime ExpectedTime { get; set; }
+    public string Status { get; set; }
+    public Flight() { }
+
+    public Flight(string flightNumber, string origin, string destination, DateTime expectedTime)
     {
+        FlightNumber = flightNumber;
+        Origin = origin;
+        Destination = destination;
+        ExpectedTime = expectedTime;
     }
+    public virtual double CalculateFees()
+    {
+        return 800.0;
+    }
+    public override string ToString()
+    {
+        return "Flight Number: " + FlightNumber + " Origin: " + Origin + " Destination: " + Destination + " Expected Time: " + ExpectedTime;
+    }
+
+}
 }
