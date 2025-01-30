a88f73411e1809d6b075751e8cb6cd359f48f8ca
joy <joywong367@gmail.com>
2025-01-30 17:46:12 +0800

    Update Airline.cs

--- Airline.cs
+++ Airline.cs
@@ -6,7 +6,55 @@ using System.Threading.Tasks;
 
 namespace S10269287_PRG2Assignment
 {
-    internal class Airline
+   class Airline
+{
+    public string Name { get; set; }
+    public string Code { get; set; }
+
+    public Dictionary<string, Flight> Flights { get; set; } = new Dictionary<string, Flight>();
+
+    public Airline() { }
+    public Airline(string name, string code)
     {
+        Name = name;
+        Code = code;
     }
+
+    public bool AddFlight(Flight flight)
+    {
+        if (Flights.ContainsKey(flight.FlightNumber))
+        {
+
+            return true;
+        }
+        Flights.Add(flight.FlightNumber, flight);
+        return false;
+    }
+
+    public double CalculateFees()
+    {
+        double fees = 0;
+        foreach (Flight flight in Flights.Values)
+        {
+            fees += flight.CalculateFees();
+
+        }
+        return fees;
+    }
+
+    public bool RemoveFlight(Flight flight)
+    {
+        if (Flights.ContainsKey(flight.FlightNumber))
+        {
+            Flights.Remove(flight.FlightNumber);
+            return true;
+        }
+        return false;
+    }
+
+    public override string ToString()
+    {
+        return "Name: " + Name + " Code: " + Code;
+    }
+}
 }
