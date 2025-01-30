c8f818d1dffb7d1e5e5d439d44a9b31e3e551d05
joy <joywong367@gmail.com>
2025-01-30 17:53:06 +0800

    Add Menu Display

--- Program.cs
+++ Program.cs
@@ -82,3 +82,23 @@ void ListBoardingGates()
 
     }
 }
+
+//Display Menu
+void DisplayMenu()
+{
+    Console.WriteLine("========================================");
+    Console.WriteLine("Welcome to Changi Airport Terminal 5");
+    Console.WriteLine("========================================");
+    Console.WriteLine("1. List All Flights");
+    Console.WriteLine("2. List Boarding Gates");
+    Console.WriteLine("3. Assign a Boarding Gate to a Flight");
+    Console.WriteLine("4. Create Flight");
+    Console.WriteLine("5. Display Airline Flights");
+    Console.WriteLine("6. Modify Flight Details");
+    Console.WriteLine("7. Display Flight Schedule");
+    Console.WriteLine("0. Exit");
+    Console.WriteLine();
+    Console.Write("Enter your choice: ");
+}
+
+
