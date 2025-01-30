f2b7e7c0a9f0b3d9fc851f95e754419f42e326fa
joy <joywong367@gmail.com>
2025-01-30 17:44:10 +0800

    Update LWTTFlight.cs

--- LWTTFlight.cs
+++ LWTTFlight.cs
@@ -6,7 +6,19 @@ using System.Threading.Tasks;
 
 namespace S10269287_PRG2Assignment
 {
-    internal class LWTTFlight
+    class LWTTFlight : Flight
+{
+    public LWTTFlight() { }
+
+    public LWTTFlight(string flightName, string origin, string destination, DateTime expectedTime) : base(flightName, origin, destination, expectedTime) { }
+    public override double CalculateFees()
     {
+        double fee = base.CalculateFees() + 500;
+        return fee;
     }
+    public override string ToString()
+    {
+        return base.ToString() + "Flight Type: LWTT";
+    }
+}
 }
