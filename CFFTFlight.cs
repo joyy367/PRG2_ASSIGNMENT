e291ee3a7b6e0e02f2b3411e429b17fb278223d0
joy <joywong367@gmail.com>
2025-02-02 23:06:57 +0800

    Correcting CFFTFlight.cs
    Corrected some mistakes and added RequestFee

--- CFFTFlight.cs
+++ CFFTFlight.cs
@@ -8,11 +8,15 @@ namespace S10269287_PRG2Assignment
 {
     class CFFTFlight : Flight
 {
+    public double RequestFee { get; set; }
     public CFFTFlight() { }
-    public CFFTFlight(string flightName, string origin, string destination, DateTime expectedTime) : base(flightName, origin, destination, expectedTime) { }
+   public CFFTFlight(string flightName, string origin, string destination, DateTime expectedTime) : base(flightName, origin, destination, expectedTime)
+    {
+        RequestFee = 150;
+    }
     public override double CalculateFees()
     {
-        double fee = base.CalculateFees() + 150;
+        double fee = base.CalculateFees() + RequestFee;
         return fee;
     }
     public override string ToString()
