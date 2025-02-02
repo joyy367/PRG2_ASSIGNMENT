7fc2814b997bd8e64926eead7b2c06e0c3f46e23
joy <joywong367@gmail.com>
2025-02-02 23:11:02 +0800

    Add Feature 5
    Added feature 5 with input validation

--- Program.cs
+++ Program.cs
@@ -183,6 +183,192 @@ void DisplayAirlineFlights()
     }
 }
 
+//Feature 5 - Assign a boarding gate to a flight
+void AssignBoardingGate()
+{
+    Console.WriteLine("===================================================");
+    Console.WriteLine("Assign Boarding Gate to a Flight");
+    Console.WriteLine("===================================================");
+
+    string flightNumber;
+    Flight flight;
+    while (true)
+    {
+        //validation of the input of flight number 
+        Console.Write("Enter Flight Number: ");
+        flightNumber = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(flightNumber))
+        {
+            Console.WriteLine("Flight Number cannot be empty!");
+            continue;
+        }
+        if (!terminal.Flights.ContainsKey(flightNumber))
+        {
+            Console.WriteLine("Invalid response. Please enter a valid Flight Number!");
+        }
+        else
+        {
+            flight = terminal.Flights[flightNumber];
+            break;
+        }
+    }
+
+    //check for special request code
+    string specialRequestCode = "None";
+    if (flight is CFFTFlight)
+    {
+        specialRequestCode = "CFFT";
+    }
+    else if (flight is DDJBFlight)
+    {
+        specialRequestCode = "DDJB";
+    }
+    else if (flight is LWTTFlight)
+    {
+        specialRequestCode = "LWTT";
+    }
+
+    BoardingGate currentAssignedGate = null;
+
+    foreach (BoardingGate gateName in terminal.BoardingGates.Values)
+    {
+        if (gateName.Flight != null && gateName.Flight.FlightNumber == flightNumber)
+        {
+            currentAssignedGate = gateName;
+            break;  // exit  the loop once the gate is found
+        }
+    }
+
+    if (currentAssignedGate != null)
+    {
+        Console.WriteLine($"The Flight {flightNumber} is currently assigned to Boarding Gate {currentAssignedGate.GateName}.");
+        Console.Write("Do you wish to reassign this flight to a new gate? (Y/N): ");
+        while (true)
+        {
+            string option = Console.ReadLine().ToUpper();
+            if (option == "Y")
+            {
+                currentAssignedGate.Flight = null;  // remove the flight assignment from the current gate
+                break;
+            }
+            else if (option == "N")
+            {
+                Console.WriteLine("The existing assignment will be kept!");
+                return;
+            }
+            else
+            {
+                Console.WriteLine("Invalid response. Please enter 'Y' or 'N'!");
+            }
+        }
+    }
+
+   
+    BoardingGate gate;
+
+    while (true)
+    {
+        Console.Write("Enter Boarding Gate: ");
+        string gateName = Console.ReadLine();
+        if (string.IsNullOrWhiteSpace(gateName))
+        {
+            Console.WriteLine("Gate Name cannot be empty!");
+            continue;
+        }
+
+        if (!terminal.BoardingGates.ContainsKey(gateName))
+        {
+            Console.WriteLine("Invalid Boarding Gate Entered. Please try again!");
+            continue;
+        }
+
+        gate = terminal.BoardingGates[gateName];
+
+        //validate if the special request codes between flights and boarding gates match
+        if ((specialRequestCode == "CFFT" && !gate.SupportsCFFT) ||
+            (specialRequestCode == "DDJB" && !gate.SupportsDDJB) ||
+            (specialRequestCode == "LWTT" && !gate.SupportsLWTT))
+        {
+            Console.WriteLine($"Boarding Gate {gateName} does not support flights with the '{specialRequestCode}' special request. Please enter a compatible gate!");
+            continue;
+        }
+        if (gate.Flight != null)
+        {
+            Console.WriteLine($"Boarding Gate {gateName} is already assigned to Flight {gate.Flight.FlightNumber}. Please enter a different Boarding Gate!");
+            continue;
+        }
+        gate.Flight = flight;
+        break;
+    }
+    Console.WriteLine($"Flight Number: {flight.FlightNumber}");
+    Console.WriteLine($"Origin: {flight.Origin}");
+    Console.WriteLine($"Destination: {flight.Destination}");
+    Console.WriteLine($"Expected Time: {flight.ExpectedTime}");
+    Console.WriteLine($"Special Request Code: {specialRequestCode}");
+    Console.WriteLine($"Boarding Gate Name: {gate.GateName}");
+    Console.WriteLine($"Supports DDJB: {gate.SupportsDDJB}");
+    Console.WriteLine($"Supports CFFT: {gate.SupportsCFFT}");
+    Console.WriteLine($"Supports LWTT: {gate.SupportsLWTT}");
+
+    //set status of flight
+    Console.Write("Would you like to update the status of the flight (Y/N): ");
+    string response = Console.ReadLine().ToUpper();
+    while (true)
+    {
+        if (response == "Y")
+        {
+            while (true)
+            {
+                Console.WriteLine("Select New Status:");
+                Console.WriteLine("1. Delayed");
+                Console.WriteLine("2. Boarding");
+                Console.WriteLine("3. On Time");
+                Console.Write("Please select the new status of the flight: ");
+                string statusChoice = Console.ReadLine();
+
+                if (statusChoice == "1")
+                {
+                    flight.Status = "Delayed";
+                    break;
+                }
+                else if (statusChoice == "2")
+                {
+                    flight.Status = "Boarding";
+                    break;
+                }
+                else if (statusChoice == "3")
+                {
+                    flight.Status = "On Time";
+                    break;
+                }
+                else if (string.IsNullOrWhiteSpace(statusChoice))
+                {
+                    Console.WriteLine("Input cannot be empty!");
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter 1, 2, or 3!");
+                }
+            }
+            break;
+        }
+        else if (response == "N")
+        {
+            Console.WriteLine("No changes for Flight Status.");
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Invalid response. Please enter 'Y' or 'N'!");
+            Console.Write("Would you like to update the Flight Status? [Y/N]: ");
+            response = Console.ReadLine().ToUpper();
+        }
+    
+    }
+    Console.WriteLine($"Flight {flight.FlightNumber} has been assigned to Boarding Gate {gate.GateName}!");
+}
+
 //Display Menu
 void DisplayMenu()
 {
